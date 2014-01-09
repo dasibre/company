e2671f6da20ef24fb1b526dbaaaf76402a3bcbfa
James <james.naadjie@gmail.com>
2014-01-09 17:51:42 -0500

    first commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Application extends Controller {
+
+  def index = TODO
+
+  def edit   = TODO
+  def create = TODO
+  def update = TODO
+  def save   = TODO
+}
